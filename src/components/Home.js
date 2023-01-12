23cafd96f8e6a28a77e3d565f88dcc118d205700
KarimCarl <cndungu109@gmail.com>
2023-01-12 13:38:02 +0300

    Completed Assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {username, city, image} from "../data/user"
+import {username, city} from "../data/user"
 
 function Home() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,12 @@ import React from "react";
 
 function NavBar() {
   return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
+     <div>
+        <nav>
+          <a href="#home">I'm a link!</a>
+        </nav>
+    </div>
+
   );
 }
 
